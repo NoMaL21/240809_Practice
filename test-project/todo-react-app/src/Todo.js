03c7ba4d3a282c8ce76e615908b3ab28e6a63919
NoMaL21 <dntjd8509@naver.com>
2024-10-08 19:36:55 +0900

    타겟 이름, 실행시간 표시

--- test-project/todo-react-app/src/Todo.js
+++ test-project/todo-react-app/src/Todo.js
@@ -1,75 +1,95 @@
 import React from 'react';
-import {ListItem, ListItemText, InputBase, Checkbox, ListItemSecondaryAction, IconButton} from "@mui/material"
-import DeleteOutlined from "@mui/icons-material/DeleteOutlined"
+import {ListItem, ListItemText, InputBase, Checkbox, ListItemSecondaryAction, IconButton} from "@mui/material";
+import DeleteOutlined from "@mui/icons-material/DeleteOutlined";
 
-class Todo extends React.Component{
-    constructor(props){
+class Todo extends React.Component {
+    constructor(props) {
         super(props);
-        this.state ={ item: props.item, readOnly:true};
+        this.state = {
+            item: props.item,
+            readOnly: true,
+        };
         this.delete = props.delete;
         this.update = props.update;
     }
 
-    deleteEventHandler =()=>{
+    deleteEventHandler = () => {
         this.delete(this.state.item);
     }
     
-    offReadOnlyMode =()=>{
-        //console.log("Event!",this.state.readOnly);
-        this.setState({readOnly:false},()=>{
-            console.log("ReadOnly?",this.state.readOnly)
-        });
+    offReadOnlyMode = () => {
+        this.setState({readOnly: false});
     }
 
-    enterKeyEventHandler =(e)=>{
-        if(e.key ==="Enter"){
-            this.setState({readOnly:true});
+    enterKeyEventHandler = (e) => {
+        if (e.key === "Enter") {
+            this.setState({readOnly: true});
             this.update(this.state.item);
         }
     }
 
-    editEventHandler =(e)=>{
+    editEventHandler = (e) => {
+        const { name, value } = e.target;
         const thisItem = this.state.item;
-        thisItem.title = e.target.value;
-        this.setState({item:thisItem});
+        thisItem[name] = value;  // 입력된 필드에 따라 title, target_name, executionTime 업데이트
+        this.setState({ item: thisItem });
     }
 
-    checkboxEventHandler =(e)=>{
-        //console.log("check box event call");
+    checkboxEventHandler = () => {
         const thisItem = this.state.item;
-        thisItem.done = thisItem.done ?false :true;
-        //this.setState({item:thisItem});
-        this.setState({readOnly:true});
+        thisItem.done = !thisItem.done;
+        this.setState({ item: thisItem, readOnly: true });
         this.update(this.state.item);
     }
 
-    render(){
-        const item =this.state.item;
+    render() {
+        const { item } = this.state;
 
-        return(
+        return (
             <ListItem>
                 <Checkbox 
                     checked={item.done}
                     onChange={this.checkboxEventHandler}
-                    />
+                />
                 <ListItemText>
-                    <InputBase
-                        inputProps={{"aria-label":"naked", readOnly:this.state.readOnly}}
-                        type="text"
-                        id={item.id}
-                        name={item.id}
-                        value={item.title}
-                        multiline={true}
-                        fullWidth={true}
-                        onClick={this.offReadOnlyMode}
-                        onChange={this.editEventHandler}
-                        onKeyPress={this.enterKeyEventHandler}
+                    <div style={{display: "flex", flexDirection: "column"}}>
+                        <InputBase
+                            inputProps={{"aria-label": "naked", readOnly: this.state.readOnly}}
+                            type="text"
+                            name="title"  // 제목 필드
+                            value={`제목 : ${item.title}`}
+                            multiline={true}
+                            fullWidth={true}
+                            onClick={this.offReadOnlyMode}
+                            onChange={this.editEventHandler}
+                            onKeyPress={this.enterKeyEventHandler}
                         />
+                        <InputBase
+                            inputProps={{"aria-label": "naked", readOnly: this.state.readOnly}}
+                            type="text"
+                            name="target_name"  // 타겟 이름 필드
+                            value={`대상 이름 : ${item.target_name}`}
+                            multiline={true}
+                            fullWidth={true}
+                            onClick={this.offReadOnlyMode}
+                            onChange={this.editEventHandler}
+                            onKeyPress={this.enterKeyEventHandler}
+                        />
+                        <InputBase
+                            inputProps={{"aria-label": "naked", readOnly: this.state.readOnly}}
+                            type="text"
+                            name="executionTime"  // 실행 시간 필드
+                            value={`시간 : ${item.executionTime}`}
+                            fullWidth={true}
+                            onClick={this.offReadOnlyMode}
+                            onChange={this.editEventHandler}
+                            onKeyPress={this.enterKeyEventHandler}
+                        />
+                    </div>
                 </ListItemText>
 
                 <ListItemSecondaryAction>
-                    <IconButton aria-label="Delete"
-                    onClick={this.deleteEventHandler}>
+                    <IconButton aria-label="Delete" onClick={this.deleteEventHandler}>
                         <DeleteOutlined/>
                     </IconButton>
                 </ListItemSecondaryAction>
@@ -77,4 +97,5 @@ class Todo extends React.Component{
         );
     }
 }
+
 export default Todo;
